0196a0e1f58ab08587d6e9e2ffe64af03286eff4
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-09-03 21:39:48 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    const getWorkouts = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/workouts`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      setWorkouts(response.data.records); 
+    }
+    getWorkouts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Working It</h1>
     </div>
   );
 }
