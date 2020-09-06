544612525861d84e4e134f24d4b33c241479e048
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-09-06 10:55:33 -0400

    updated add form styles

--- src/components/CreateEntry.jsx
+++ src/components/CreateEntry.jsx
@@ -17,6 +17,7 @@ const AddHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  height: 60px;
 `
 const AddTitle = styled.h3`
   color: white;
@@ -24,11 +25,50 @@ const AddTitle = styled.h3`
 `
 
 const AddForm = styled.form`
-  text-align: left;
-  padding: 30px;
   display: flex;
   flex-direction: column;
+  padding: 5px 25px 25px 25px;
 `
+
+const DateInput = styled.div`
+  margin-bottom: 20px;
+`
+
+const ExerciseInput = styled.div`
+  margin-bottom: 20px;
+  width: 275px;
+`
+
+const Duration = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`
+
+const DurationLabel = styled.h4`
+  margin-bottom: 5px;
+`
+
+const DurationInputs = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const StatusInput = styled.div`
+  margin-bottom: 20px;
+  width: 275px;
+`
+
+
+const dropdownStyle = {
+  menu: (provided, selector) => ({
+    ...provided,
+    width: 300,
+    color: selector.selectProps.menuColor,
+    padding: 20,
+  })
+}
+
 
 const CreateEntry = (props) => {
   const [date, setDate] = useState("");
@@ -45,6 +85,16 @@ const CreateEntry = (props) => {
     { value: 'Cycle', label: 'Cycle' },
     { value: 'Run', label: 'Run' },
     { value: 'Swim', label: 'Swim' },
+    { value: 'Walk', label: 'Walk' },
+    { value: 'Rower', label: 'Rower' },
+    { value: 'Elliptical', label: 'Elliptical' },
+    { value: 'High Intensity Interval Training', label: 'High Intensity Interval Training' },
+    { value: 'Stair Stepper', label: 'Stair Stepper' },
+    { value: 'Hiking', label: 'Hiking' },
+    { value: 'Yoga', label: 'Yoga' },
+    { value: 'Dance', label: 'Dance' },
+    { value: 'Strength Training', label: 'Strength Training' },
+    { value: 'Other', label: 'Other' },
   ];
 
   for (let key in exercise) {
@@ -99,62 +149,101 @@ const CreateEntry = (props) => {
         <AddTitle>Add Workout</AddTitle>
       </AddHeader>
       <AddForm onSubmit={handleSubmit}>
-        <label htmlFor="date">Date:</label>
-        <input
-          type="date"
-          name="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />      
-        <label htmlFor="exercise">Exercise:</label>
-        <Select
-          name="exercise"
-          defaultValue={exercise}
-          options={exerciseOptions}        
-          onChange={setExercise}
-        />
-        <h4>Duration:</h4>
-        <label htmlFor="hours">Hours:</label>
-        <input
-          type="number"
-          name="hours"
-          min="0"
-          max="24"
-          value={hours}
-          onChange={(e) => setHours(e.target.value)}
-        />      
-        <label htmlFor="minutes">Minutes:</label>
-        <input
-          type="number"
-          name="minutes"
-          min="0"
-          max="59"        
-          value={minutes}
-          onChange={(e) => setMinutes(e.target.value)}
-        />  
-        <label htmlFor="seconds">Seconds:</label>
-        <input
-          type="number"
-          name="seconds"
-          min="0"
-          max="59"         
-          value={seconds}
-          onChange={(e) => setSeconds(e.target.value)}
-        />       
-        <label htmlFor="status">Status:</label>
-        <Select
-          defaultValue={status}
-          options={statusOptions}        
-          onChange={setStatus}
-        />
-        <label htmlFor="notes">Notes:</label>
-        <input
-          type="text"
-          name="notes"
-          value={notes}
-          onChange={(e) => setNotes(e.target.value)}
-        /> 
-        <button className="add-button" type="submit">Add</button>
+        <DateInput>
+          <div>
+            <label htmlFor="date">Date</label>
+          </div>
+          <div>
+            <input
+            type="date"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            />                       
+          </div>
+        </DateInput>
+        <ExerciseInput>
+          <div>
+            <label htmlFor="exercise">Exercise</label>
+          </div>
+          <div>
+            <Select
+              name="exercise"
+              styles={dropdownStyle}
+              menuColor='black'
+              defaultValue={exercise}
+              options={exerciseOptions}        
+              onChange={setExercise}
+            />                      
+          </div>
+        </ExerciseInput>
+        <Duration>
+          <div>
+            <DurationLabel>Duration</DurationLabel>
+          </div>
+          <DurationInputs>
+            <label htmlFor="hours">Hours</label>
+            <input
+              type="number"
+              name="hours"
+              min="0"
+              max="24"
+              value={hours}
+              onChange={(e) => setHours(e.target.value)}
+            />      
+            <label htmlFor="minutes">Minutes</label>
+            <input
+              type="number"
+              name="minutes"
+              min="0"
+              max="59"        
+              value={minutes}
+              onChange={(e) => setMinutes(e.target.value)}
+            />  
+            <label htmlFor="seconds">Seconds</label>
+            <input
+              type="number"
+              name="seconds"
+              min="0"
+              max="59"         
+              value={seconds}
+              onChange={(e) => setSeconds(e.target.value)}
+            />                 
+          </DurationInputs>  
+        </Duration>
+        <StatusInput>
+          <div>
+            <label htmlFor="status">Status</label>
+          </div>
+          <div>
+            <Select
+              name="status"
+              styles={dropdownStyle}
+              // width=300
+              menuColor='black'
+              defaultValue={status}
+              options={statusOptions}        
+              onChange={setStatus}
+            />              
+          </div>
+        </StatusInput>
+        <div>
+          <div>
+            <label htmlFor="notes">Notes</label>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="notes"
+              value={notes}
+              onChange={(e) => setNotes(e.target.value)}
+            />             
+          </div>
+        </div>
+        <div>
+          <button type="reset">Cancel</button>
+          <button type="submit">Add</button> 
+        </div>
       </AddForm>  
     </AddContainer>  
   )
